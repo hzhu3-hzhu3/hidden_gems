e68564eb21151db5629eb8e9d314869c46534ff1
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-03-23 16:35:46 -0500

    feat: cart can edit quantity and remove items separately

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,4 +15,25 @@ class CartController < ApplicationController
     session[:cart][id] += 1
     redirect_to cart_path
   end
+
+  def update
+    session[:cart] ||= {}
+    product_id = params[:id].to_s
+    new_qty = params[:quantity].to_i
+  
+    if new_qty <= 0
+      session[:cart].delete(product_id)
+    else
+      session[:cart][product_id] = new_qty
+    end
+  
+    redirect_to cart_path
+  end
+  
+  def remove
+    session[:cart] ||= {}
+    session[:cart].delete(params[:id].to_s)
+    redirect_to cart_path
+  end
+  
 end
