f8e3de32bb9ec5f287660c79ac1bf74d1bd7d54d
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-03-23 16:33:38 -0500

    feat: category show page displays products from that category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,10 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1 or /categories/1.json
   def show
+    @category = Category.find(params[:id])
+    @products = @category.products
   end
+  
 
   # GET /categories/new
   def new
