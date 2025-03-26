7c954a37bb6df05bef6a8aaf6a14d57dde60f126
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-03-25 23:07:28 -0500

    Update products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,30 +3,31 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :check_admin, except: [:index, :show]
 
-  # GET /products or /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1 or /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
-
+  
     respond_to do |format|
       if @product.save
+        ProductPrice.create!(
+          product: @product,
+          price: params[:initial_price],
+          effective_date: Time.current
+        )
+  
         format.html { redirect_to @product, notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @product }
       else
@@ -35,8 +36,8 @@ class ProductsController < ApplicationController
       end
     end
   end
+  
 
-  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
@@ -49,7 +50,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy!
 
@@ -60,15 +60,14 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
+
     def product_params
-      params.require(:product).permit(:name, :description, :photo)
-    end
+      params.require(:product).permit(:name, :description, :price, :photo)
+    end    
 
     def check_admin
       redirect_to root_path, alert: "Access denied" unless current_user&.admin?
