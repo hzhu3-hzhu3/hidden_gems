7f931e15a19b323e94a8b3d6e1a80d9d5031b418
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-04-10 10:31:12 -0500

    update sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :set_cart_count
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def set_cart_count
     @cart_count = session[:cart].present? ? session[:cart].size : 0
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
