e52c20c98f5d413255c14b0eb33acb279e42172c
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-04-16 11:55:12 -0500

    Improve admin dashboard styling to match frontend design

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -3,7 +3,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const deleteLinks = document.querySelectorAll('.delete-link, a[data-method="delete"]');
   deleteLinks.forEach(function(link) {
-
     link.setAttribute('data-turbo', 'false');
     
     link.addEventListener('click', function(e) {
@@ -33,44 +32,47 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-
-    const editors = document.querySelectorAll('.quill-editor');
-    if(editors.length > 0) {
-      editors.forEach(function(editor) {
-        const container = document.createElement('div');
-        container.className = 'quill-editor-container';
-        container.style.height = '300px';
-        container.style.marginBottom = '15px';
-        editor.parentNode.insertBefore(container, editor);
-        
-        editor.style.display = 'none';
-        
-        const quill = new Quill(container, {
-          theme: 'snow',
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],
-              ['blockquote', 'code-block'],
-              [{ 'header': 1 }, { 'header': 2 }],
-              [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-              [{ 'script': 'sub' }, { 'script': 'super' }],
-              [{ 'indent': '-1' }, { 'indent': '+1' }],
-              [{ 'size': ['small', false, 'large', 'huge'] }],
-              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-              [{ 'color': [] }, { 'background': [] }],
-              [{ 'font': [] }],
-              [{ 'align': [] }],
-              ['clean'],
-              ['link', 'image']
-            ]
-          }
-        });
-        
-        quill.root.innerHTML = editor.value;
-
-        editor.form.addEventListener('submit', function() {
-          editor.value = quill.root.innerHTML;
-        });
+  
+  const editors = document.querySelectorAll('.quill-editor');
+  if(editors.length > 0) {
+    editors.forEach(function(editor) {
+      const container = document.createElement('div');
+      container.className = 'quill-editor-container';
+      editor.parentNode.insertBefore(container, editor);
+      
+      editor.style.display = 'none';
+      
+      const quill = new Quill(container, {
+        theme: 'snow',
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ 'header': 1 }, { 'header': 2 }],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'align': [] }],
+            ['clean'],
+            ['link', 'image']
+          ]
+        }
       });
-    }
-  });
+      
+      quill.root.innerHTML = editor.value;
+      
+      editor.form.addEventListener('submit', function() {
+        editor.value = quill.root.innerHTML;
+      });
+    });
+  }
+  
+  const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  if (tooltips.length > 0 && typeof bootstrap !== 'undefined') {
+    tooltips.forEach(tooltip => {
+      new bootstrap.Tooltip(tooltip);
+    });
+  }
+});
