d64f774d5296a56e8e47689024fbe8aff9fe1d77
hzhu3-hzhu <hzhu3@academic.rrc.ca>
2025-04-23 15:56:04 -0500

    style(admin): reuse bootstrap utilities in active_admin_custom.scss

---
+++ bin/build_admin_css.sh
@@ -0,0 +1,9 @@
+sass app/assets/stylesheets/active_admin.scss app/assets/builds/active_admin.css \
+  --style=compressed \
+  --load-path=/home/gloria10/.rbenv/versions/3.2.2/lib/ruby/gems/3.2.0/gems/activeadmin-3.2.5/app/assets/stylesheets \
+  --load-path=node_modules
+
+sass app/assets/stylesheets/active_admin_custom.scss app/assets/builds/active_admin_custom.css \
+  --style=compressed \
+  --load-path=/home/gloria10/.rbenv/versions/3.2.2/lib/ruby/gems/3.2.0/gems/activeadmin-3.2.5/app/assets/stylesheets \
+  --load-path=node_modules
